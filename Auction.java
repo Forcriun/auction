e29265f46fc27a94f5b1ba2ec1ce5a9166e24a2a
Forcriun <forcriun@gmail.com>
2018-02-01 16:57:19 +0100

    Añadido metodo close que muestra informacion de pujas de los lotes

--- Auction.java
+++ Auction.java
@@ -43,7 +43,7 @@ public class Auction
             System.out.println(lot.toString());
         }
     }
-    
+
     /**
      * Make a bid for a lot.
      * A message is printed indicating whether the bid is
@@ -60,14 +60,14 @@ public class Auction
             boolean successful = selectedLot.bidFor(new Bid(bidder, value));
             if(successful) {
                 System.out.println("The bid for lot number " +
-                                   lotNumber + " was successful.");
+                    lotNumber + " was successful.");
             }
             else {
                 // Report which bid is higher.
                 Bid highestBid = selectedLot.getHighestBid();
                 System.out.println("Lot number: " + lotNumber +
-                                   " already has a bid of: " +
-                                   highestBid.getValue());
+                    " already has a bid of: " +
+                    highestBid.getValue());
             }
         }
     }
@@ -86,9 +86,9 @@ public class Auction
             // right lot.
             if(selectedLot.getNumber() != lotNumber) {
                 System.out.println("Internal error: Lot number " +
-                                   selectedLot.getNumber() +
-                                   " was returned instead of " +
-                                   lotNumber);
+                    selectedLot.getNumber() +
+                    " was returned instead of " +
+                    lotNumber);
                 // Don't return an invalid lot.
                 selectedLot = null;
             }
@@ -96,8 +96,21 @@ public class Auction
         }
         else {
             System.out.println("Lot number: " + lotNumber +
-                               " does not exist.");
+                " does not exist.");
             return null;
+        }
+    }
+
+    public void close(){
+        for(Lot lot : lots) {
+            Bid puja = lot.getHighestBid();
+            System.out.println(lot.toString());
+            if(puja == null){
+                System.out.println("El lote nº " + lot.getNumber() + " no contiene pujas."); 
+            }
+            else{
+                System.out.println("El lote nº " + lot.getNumber() + " tiene una puja más alta de " + puja.getValue() + " realizada por " + puja.getBidder().getName() + ".");                
+            }
         }
     }
 }
